19092e63460a552a14a30c76461e33bd1ef7f281
Jeff Hollister <jeff.w.hollister@gmail.com>
2014-10-31 11:13:53 -0400

    added text on conditional accuracy and start of conditional accuracy function

---
+++ R/condAccuracy.R
@@ -0,0 +1,12 @@
+#' Plot conditional accuracy
+#' 
+#' This function plots the total accuracy as a function of maximum probability. 
+#' We determine the maximum probability for each lake for a given random forest
+#' object and then plot the total accuracy for lakes at each value of x or greater
+#' 
+#' @param rf A random forest object
+#' @param ... parameters to be passed to condprob
+#' @export
+condAccuracy<-function(rf,...){
+  
+}
