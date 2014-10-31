3e5e68851ec5f8b3d27ecdd4be5b7319ac93833d
Jeff Hollister <jeff.w.hollister@gmail.com>
2014-10-31 15:37:07 -0400

    added conditional accuracy figure

--- R/condAccuracy.R
+++ R/condAccuracy.R
@@ -7,6 +7,29 @@
 #' @param rf A random forest object
 #' @param ... parameters to be passed to condprob
 #' @export
-condAccuracy<-function(rf,...){
+#' @import condprob2 ggplot2
+#' @examples
+#' condAccuracy(gis_ts4_rf,xImpair=0,R=1,xlab="Maximum Vote Probability")
+condAccuracy<-function(rf,xlab="x",...){
+  max_vote<-apply(rf$votes,1,max)
+  obs<-rf$y
+  pred<-factor(rf$predicted,levels=unique(obs),ordered=is.ordered(obs))
+  correct<-pred==obs
+  cp<-condprob(max_vote,correct,ProbComp="gt",Exceed="gte",...)
+  #plot(cp$max_vote,cp$Raw.Data.Probability)
+  cp<-data.frame(cp$max_vote,cp$Raw.Data.Probability)
   
+  ggplot(cp,aes(x=cp.max_vote,y=cp.Raw.Data.Probability))+
+    geom_point()+
+    theme(text = element_text(family="serif"),
+          panel.background = element_blank(), #panel.grid = element_blank(), 
+          panel.border = element_rect(fill = NA), 
+          plot.title  = element_text(family="serif",size=22,face="bold",vjust=1.1),
+          legend.position = "none", legend.key = element_rect(fill = 'white'),
+          legend.text = element_text(family="serif",size=15), legend.title = element_text(size=15),
+          axis.title.x = element_text(family="serif",vjust = -0.5, size = 20),
+          axis.title.y = element_text(family="serif",vjust = 1.5, size = 20),
+          axis.text.x = element_text(family="serif",size = 15),
+          axis.text.y = element_text(family="serif",size = 15)) + 
+    labs(y="Conditional Total Accuracy",x=xlab)
 }
