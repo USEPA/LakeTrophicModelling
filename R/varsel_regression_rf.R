5d4ab5fef6487e6aa68bb0c00949cd7dcd94b342
Jeff Hollister <jeff.w.hollister@gmail.com>
2015-06-10 11:50:46 -0400

    finishing touches on varsel

--- R/varsel_regression_rf.R
+++ R/varsel_regression_rf.R
@@ -12,14 +12,30 @@
 #' data(LakeTrophicModelling)
 #' predictors_all <- predictors_all[predictors_all!="DATE_COL"]
 #' all_dat <- data.frame(ltmData[predictors_all],LogCHLA=log10(ltmData$CHLA))
-#' all_dat <- all_cf_dat[complete.cases(all_cf_dat),]
-#' x<-varsel_regression_rf(all_dat$LogCHLA,all_cf_dat[,names(all_cf_dat)!="LogCHLA"])
+#' all_dat <- all_dat[complete.cases(all_dat),]
+#' x<-varsel_regression_rf(all_dat$LogCHLA,all_dat[,names(all_dat)!="LogCHLA"],
+#'                         ntree=100)
 varsel_regression_rf <- function(y,x,...){
-  out <- list()
+  out <- list(mse=NULL,rsq=NULL,num_var=NULL,vars=NULL)
   dat <- data.frame(y=y,x)
-  init_rf <- randomForest(y~.,data=dat,...)
+  init_rf <- randomForest(y=dat$y,x=dat[,names(x)],...)
   init_imp <- importance(init_rf)
-  browser()
-  for(i in 1:n){
+  var_sort <- rownames(init_imp)[order(init_imp[,1],decreasing = TRUE)]
+  vars <- NULL
+  for(i in var_sort){
+    if(is.null(vars)){ 
+      vars <- c(vars,i) 
+      idx <- 1
+    } else {
+      vars <- c(vars,i)
+      vars_rf <- randomForest(y=dat$y,x=dat[,vars],...)
+      out$mse[[idx]] <- vars_rf$mse[length(vars_rf$mse)]
+      out$rsq[[idx]] <- vars_rf$rsq[length(vars_rf$rsq)]
+      out$num_var[[idx]] <- length(vars)
+      out$vars[[idx]] <- vars
+      idx <- idx + 1
+    }
+    print(paste(i/length(var_sort),"% completed"))
   }
+  return(out)
 }
