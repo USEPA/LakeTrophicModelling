d478d5456a1e44a54bc88ddf44eca75f6b8a4a0e
Jeff Hollister <jeff.w.hollister@gmail.com>
2015-06-11 15:34:09 -0400

    more changes

--- R/varImportance.R
+++ R/varImportance.R
@@ -1,19 +1,19 @@
 #' Variable Importance Summary
 #' 
-#' @param rf Random Forest Object
+#' @param rf_imp a Random Forest Importance Object
 #' @param label A label for the model
 #' @export
-varImportance<-function(rf,label=NULL){
+varImportance<-function(rf_imp,label=NULL){
   if(!is.null(label)){
-    xdf<-data.frame(model_id=rep(label,dim(rf$importance)[1]),
-             variable_names=row.names(rf$importance),
-             mean_decrease_acc=data.frame(rf$importance)$MeanDecreaseAccuracy,
-             mean_decrease_gini=data.frame(rf$importance)$MeanDecreaseGini)
+    xdf<-data.frame(model_id=rep(label,nrow(rf_imp)),
+             variable_names=row.names(rf_imp),
+             mean_decrease_acc=rf_imp[,1],
+             mean_decrease_gini=rf_imp[,2])
   } else {
-    xdf<-data.frame(model_id=rep(NA,dim(rf$importance)[1]),
-                    variable_names=row.names(rf$importance),
-                    mean_decrease_acc=data.frame(rf$importance)$MeanDecreaseAccuracy,
-                    mean_decrease_gini=data.frame(rf$importance)$MeanDecreaseGini)
+    xdf<-data.frame(model_id=rep(NA,nrow(rf_imp),
+                                 variable_names=row.names(rf_imp)),
+                                 mean_decrease_acc=rf_imp[,1],
+                                 mean_decrease_gini=rf_imp[,2])
   }   
   return(xdf)
 }
