d21a1fbe8eed429a0f3807a2d2585ea0033bb4a8
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2015-07-09 13:55:27 -0400

    rendered and edits up to line 283

--- R/varImportance.R
+++ R/varImportance.R
@@ -4,14 +4,15 @@
 #' @param label A label for the model
 #' @export
 varImportance<-function(rf_imp,label=NULL){
+
   if(!is.null(label)){
     xdf<-data.frame(model_id=rep(label,nrow(rf_imp)),
              variable_names=row.names(rf_imp),
              mean_decrease_acc=rf_imp[,1],
              mean_decrease_gini=rf_imp[,2])
   } else {
-    xdf<-data.frame(model_id=rep(NA,nrow(rf_imp),
-                                 variable_names=row.names(rf_imp)),
+    xdf<-data.frame(model_id=rep(NA,nrow(rf_imp)),
+                                 variable_names=row.names(rf_imp),
                                  mean_decrease_acc=rf_imp[,1],
                                  mean_decrease_gini=rf_imp[,2])
   }   
