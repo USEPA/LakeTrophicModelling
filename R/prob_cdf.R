4ea5773c50a49fcc25ff0874fd7f250b623e4c9e
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2015-07-21 16:29:44 -0400

    started work on cdf

--- R/prob_cdf.R
+++ R/prob_cdf.R
@@ -6,26 +6,26 @@
 #' 
 #' @param probs the continuous Variable such as-'sumBioV','CHLA', 'NTL', 'PTL', etc.  Of same 
 #'                length as contVar 
-#' @param cdf_colors colors for CDF plots and median line -should equal # of categories in catVar + 1
 #' @param ... pass additional parameters to labs (e.g. set titles, axis labels, etc.)
 #' 
 #' @examples
 #' devtools::install_github('wesanderson','karthik')
 #' library(wesanderson)
 #' data(LakeTrophicModelling)
-#' 
+#' prob_cdf(gis_rf_ts_prob)
 #' @export
 #' @import ggplot2
 
-prob_cdf <- function(probs cdf_colors = 1:length(catVar), ...) {
-
+prob_cdf <- function(probs, ...) {
+    browser()
     options(scipen = 5)  #tell r not to use scientific notation on axis labels
-    
-    keep<-complete.cases(catVar,contVar)
-    cdf_df<-data.frame(catVar,contVar)[keep,]
-    x <- ggplot(cdf_df,aes(contVar,colour=catVar)) +
-          stat_ecdf(size=2)+scale_color_manual(values=cdf_colors)+
-          scale_x_log10()+
+    probs$max <- apply(probs[,1:4],1,max)
+    x <- ggplot(probs) +
+          stat_ecdf(color = "black", size = 3 , aes(max)) +
+          stat_ecdf(color = "blue", size = 3 , aes(oligo)) +
+          stat_ecdf(color = "yellow", size = 3 , aes(meso)) +
+          stat_ecdf(color = "orange", size = 3 , aes(eu)) +
+          stat_ecdf(color = "red", size = 3 , aes(hyper)) +
           theme(text = element_text(family="Times"),
                 panel.background = element_blank(), panel.grid = element_blank(), 
                 panel.border = element_rect(fill = NA), 
